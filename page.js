bcfc880c08415428439b6d4e2da18be3021d8c9c
Gagan Kumar Gupta <98738303+gaganald@users.noreply.github.com>
2023-09-17 20:29:46 +0530

    Add files via upload

---
+++ page.js
@@ -0,0 +1,53 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+import { ImSearch } from "react-icons/im";
+
+const api_url = "http://www.omdbapi.com/?i=tt3896198&apikey=678fd0a4";
+const page = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const searchMovie = async (title) => {
+    const response = await fetch(`${api_url}&s=${title}`);
+    const data = await response.json();
+    console.log(data)
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovie("spiderman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <div className="searchIcon" onClick={() => searchMovie(searchTerm)}>
+          <ImSearch />
+        </div>
+      </div>
+      
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default page;
+
